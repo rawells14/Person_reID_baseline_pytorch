c233b337b751e2428310ab54bde7d5e531ccccf6
Ryan Wells <rawells14@tamu.edu>
2018-11-30 01:11:27 -0600

    dense net script

--- runDenseNet.sh
+++ runDenseNet.sh
@@ -1,1 +1,1 @@
-python3 train.py --use_dense --name DenseNet-121 --batchsize 16 --name DenseNet --data_dir /datasets/Market-1501-Segmented-Baseline/pytorch --train_all
+python3 train.py --use_dense --name DenseNet-121 --batchsize 24 --name DenseNet --data_dir /datasets/Market-1501-Segmented-Baseline/pytorch --train_all
